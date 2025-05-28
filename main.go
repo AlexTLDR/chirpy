76fd41400c87a88dc38c1633547fbe79e9954da0
AlexTLDR <alex.badragan@protonmail.com>
2025-05-28 16:00:08 +0200

    replaced w.Write with Fprintf

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
